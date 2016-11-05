feb0f37d28395321541ae86fb03af300440dff46
RINDO <rindo.katsura@gmail.com>
2016-11-06 00:08:43 +0900

    fixed rsync

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -avr --exclude='.bundle/' --exclude='vendor/' --exclude='node_modules' ./htdocs/ rindo@rindo.me:/home/rindo/html/
+rsync -avr --delete --exclude='.bundle/' --exclude='vendor/' --exclude='node_modules' ./htdocs/ rindo@rindo.me:/home/rindo/html/
