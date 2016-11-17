e2ba9f37728040b0c0f8aa384ee74bfb42f2c56a
RINDO <rindo.katsura@gmail.com>
2016-11-17 15:56:23 +0900

    css hash

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
-const gulp    = require("gulp");
-const plumber = require("gulp-plumber");
-const slim    = require("gulp-slim");
-const sass    = require("gulp-sass");
-const browser = require("browser-sync");
-const prefix  = require("gulp-autoprefixer")
+const gulp       = require("gulp");
+const plumber    = require("gulp-plumber");
+const slim       = require("gulp-slim");
+const sass       = require("gulp-sass");
+const browser    = require("browser-sync");
+const prefix     = require("gulp-autoprefixer")
 const browserify = require("gulp-browserify");
+const rename     = require("gulp-rename");
+const del        = require("del");
+const fs         = require("fs");
 
 // config
 const config = {
   path : {
-    slim        : "./src/**/*.slim",
-    target_sass : "./src/sass/main.scss",
-    sass_files  : "./src/**/*.scss",
-    target_js   : "./src/javascript/app.js",
-    javascripts : "./src/javascript/*.js"
+    slim          : "./src/*.slim",
+    slim_includes : "./src/includes/",
+    target_sass   : "./src/sass/main.scss",
+    sass_files    : "./src/**/*.scss",
+    target_js     : "./src/javascript/app.js",
+    javascripts   : "./src/javascript/*.js"
   },
   out : {
-    slim : "./html/",
-    sass : "./html/css/",
+    html : "./html/",
+    css : "./html/css/",
     all  : "./html/**",
     javascript: "./html/js/"
   }
@@ -45,7 +49,7 @@ gulp.task('browserify', () => {
       .pipe(gulp.dest(config.out.javascript));
 });
 
-gulp.task('js:watch', ['browserify'], () => {
+gulp.task('js:watch', () => {
   gulp.watch(config.path.javascripts, ['browserify']);
 });
 
@@ -58,10 +62,10 @@ gulp.task('slim', () => {
         require: 'slim/include',
         options: 'include_dirs=["./src/includes"]'
       }))
-      .pipe(gulp.dest(config.out.slim))
+      .pipe(gulp.dest(config.out.html))
 });
 
-gulp.task('slim:watch', ['slim'], () => {
+gulp.task('slim:watch', () => {
   gulp.watch(config.path.slim, ['slim']);
 });
 
@@ -74,14 +78,30 @@ gulp.task('sass', () => {
             browsers: ['last 2 version', 'iOS >= 8.1', 'Android >= 4.4'],
             cascade: false
         }))
-      .pipe(gulp.dest(config.out.sass))
+      .pipe(gulp.dest(config.out.css))
 });
 
-gulp.task('sass:watch', ['sass'], () => {
-  gulp.watch(config.path.sass_files, ['sass']);
+// rename
+gulp.task('rename:build', () => {
+  // rename
+  del.sync([config.out.css + "main.css?*"]);
+
+  let css = "main.css?" + Date.now();
+  gulp.src(config.out.css + "main.css")
+      .pipe(rename(css))
+      .pipe(gulp.dest(config.out.css));
+
+  let header = config.path.slim_includes + "header.slim";
+  fs.readFile(header, 'utf8', (err, data) => {
+    fs.writeFileSync(header, data.replace(/main\.css\?.*/gi, css+'"'), 'utf8');
+  });
 });
 
-gulp.task('html:watch', ['reload'], () => {
+gulp.task('sass:watch', () => {
+  gulp.watch(config.path.sass_files, ['sass', 'rename:build', 'slim']);
+});
+
+gulp.task('html:watch', () => {
   gulp.watch(config.out.all, ['reload']);
 });
 
@@ -89,4 +109,4 @@ gulp.task('html:watch', ['reload'], () => {
 gulp.task('watch', ['browser-sync' ,'slim:watch', 'sass:watch', 'html:watch', 'js:watch']);
 
 // task
-gulp.task("default", ["slim", "sass"]);
+gulp.task("default", ["slim", "sass", "rename:build"]);
